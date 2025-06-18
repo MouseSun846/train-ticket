c30f30d8ca948d2ff5f590fab806b6495abfc381
3026098675@qq.com <3026098675@qq.com>
2025-06-18 20:42:18 +0800

    arm

--- script/publish-docker-images.sh
+++ script/publish-docker-images.sh
@@ -2,14 +2,23 @@
 set -eux
 
 echo
-echo "Publishing images, Repo: $1, Tag: $2"
+echo "Publishing ARM64 images, Repo: $1, Tag: $2"
 echo
+
+# 默认使用 ARM64 架构构建
+PLATFORM="linux/arm64"
+
 for dir in ts-*; do
     if [[ -d $dir ]]; then
         if [[ -n $(ls "$dir" | grep -i Dockerfile) ]]; then
-            echo "build ${dir}"
-	    # Must use `buildx` as docker build tool
-            docker build --push -t "$1"/"${dir}":"$2" "$dir"
+            echo "Building ${dir} for platform ${PLATFORM}"
+
+            # 使用 buildx 构建并推送 ARM64 镜像
+            docker buildx build \
+              --platform "${PLATFORM}" \
+              --push \
+              -t "$1/${dir}:$2" \
+              "$dir"
         fi
     fi
 done
